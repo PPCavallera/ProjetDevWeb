31fce72478b8c646c00f0a282387dd0654a97d3d
PPCavallera <pierre.paul.cavallera@gmail.com>
2023-01-28 16:35:48 +0100

    implémentation du bot

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,21 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
- 
+import transformers
+from transformers import BloomForCausalLM
+from transformers import BloomTokenizerFast
+import torch
+
+model = BloomForCausalLM.from_pretrained("bigscience/bloom-560m")
+tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom-560m")
+chosen_prompt = 2
+prompt = ["Travailleurs travailleuses on nous spolie, on nous ment", 
+        "Je suis un chien déguisé en singe Je n’ai rien d’un humain", 
+        "Montre en diamants, lunettes de soleil Sors les Kalash' comme à Marseille Ma question préférée : qu'est-ce j'vais faire de tout cet oseille ?"]
+result_length = 120
+
+
+
 app = Flask(__name__)
 # creates SQLALCHEMY object
 
@@ -37,12 +51,23 @@ def test():
     print(request.args.get('conv_id'))
     question = Message(content=request.args.get('question'),
                        conv_id=request.args.get('conv_id'))
-    answer = Message(content="test réponse",
+    prompt_value = prompt[chosen_prompt] + " " + request.args.get('question')
+    inputs = tokenizer(prompt_value, return_tensors="pt")
+    answer_str = tokenizer.decode(model.generate(inputs["input_ids"],
+                       max_length=result_length, 
+                       num_beams=1, 
+                       no_repeat_ngram_size=2,
+                       early_stopping=True
+                      )[0])
+    # print(prompt_value)
+    answer_str = answer_str[len(prompt_value):]
+    print(answer_str)
+    answer = Message(content=answer_str,
                      conv_id=request.args.get('conv_id'))
     db.session.add(question)
     db.session.add(answer)
     db.session.commit()
-    return "test réponse"
+    return answer_str
 
 
 @app.route('/api/load_messages')
